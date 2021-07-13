81cab7e8fcf0b5f492ee13f0533800c93c4c49cd
Chris J. Karr <chris@audacious-software.com>
2021-07-13 15:11:47 -0500

    Cleaning up unused import

--- circle_settings.py
+++ circle_settings.py
@@ -93,9 +93,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
-ADMINS = []
-ALLOWED_HOSTS = [ '*' ]
-
 DATABASES = {
     'default': {
         'ENGINE':   'django.contrib.gis.db.backends.postgis',
